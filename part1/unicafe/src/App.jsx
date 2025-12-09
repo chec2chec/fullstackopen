ac6d7ebfe837a5e8717abd39c23b89ce73fb05be
Ivan Simeonov <ivansimeonoff@gmail.com>
2025-12-09 22:56:39 +0200

    1.10: unicafe step 5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,22 +1,5 @@
 import { useState } from 'react'
 
-const Statistics = (props) => {
-  if (props.good == 0 && props.neutral == 0 && props.bad == 0) {
-    return (
-      <div>No feedback given</div>
-    )
-  }
-  return (
-      <div>
-        good {props.good}<br/>
-        neutral {props.neutral}<br/>
-        bad {props.bad}<br/>
-        average {((props.good * 1) + (props.neutral * 0) + (props.bad * -1)) / (props.good + props.neutral + props.bad)}<br/>
-        positive {(props.good / (props.good + props.neutral + props.bad)) * 100} %
-      </div>
-  )
-}
-
 function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -45,6 +28,29 @@ function App() {
       <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
+  )
+}
+
+const Statistics = (props) => {
+  if (props.good == 0 && props.neutral == 0 && props.bad == 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+  return (
+      <div>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        average {((props.good * 1) + (props.neutral * 0) + (props.bad * -1)) / (props.good + props.neutral + props.bad)}<br/>
+        positive {(props.good / (props.good + props.neutral + props.bad)) * 100} %
+      </div>
+  )
+}
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <div>{text} {value}</div>
   )
 }
 
